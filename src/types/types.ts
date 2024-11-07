331bc082fa50ae792da2d7b6329e842fb7d7b4a5
Mike S <mksoofian@gmail.com>
2024-11-07 11:19:37 -0500

    added weatherCodes to types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -20,10 +20,38 @@ export type WeatherData = {
   };
 };
 
-// Conversion of type
-// '{ current: { time: Date; temperature2m: number; apparentTemperature: number; isDay: number; };
-//    hourly: { time: Date[]; temperature2m: Float32Array; apparentTemperature: Float32Array; precipitationProbability: Float32Array; precipitation: Float32Array; };
-//    daily: { ...; }; }'
-// to type 'WeatherData' may be a mistake because neither type sufficiently overlaps with the other. If this was intentional, convert the expression to 'unknown' first.
-//   Types of property 'daily' are incompatible.
-//     Type '{ time: Date[]; weatherCode: Float32Array; temperature2mMax: Float32Array; temperature2mMin: Float32Array; }' is missing the following properties from type '{ time: Date; temperature2m: Float32Array; temperature2mMax: Float32Array; temperature2mMin: Float32Array; precipitationProbability: Float32Array; }': temperature2m, precipitationProbabilityts(2352)
+type WeatherCode = {
+  code: number;
+  description: string;
+};
+
+export const weatherCodes: WeatherCode[] = [
+  { code: 0, description: "Clear sky" },
+  { code: 1, description: "Mainly clear, partly cloudy, and overcast" },
+  { code: 2, description: "Mainly clear, partly cloudy, and overcast" },
+  { code: 3, description: "Mainly clear, partly cloudy, and overcast" },
+  { code: 45, description: "Fog and depositing rime fog" },
+  { code: 48, description: "Fog and depositing rime fog" },
+  { code: 51, description: "Drizzle: Light, moderate, and dense intensity" },
+  { code: 53, description: "Drizzle: Light, moderate, and dense intensity" },
+  { code: 55, description: "Drizzle: Light, moderate, and dense intensity" },
+  { code: 56, description: "Freezing Drizzle: Light and dense intensity" },
+  { code: 57, description: "Freezing Drizzle: Light and dense intensity" },
+  { code: 61, description: "Rain: Slight, moderate and heavy intensity" },
+  { code: 63, description: "Rain: Slight, moderate and heavy intensity" },
+  { code: 65, description: "Rain: Slight, moderate and heavy intensity" },
+  { code: 66, description: "Freezing Rain: Light and heavy intensity" },
+  { code: 67, description: "Freezing Rain: Light and heavy intensity" },
+  { code: 71, description: "Snow fall: Slight, moderate, and heavy intensity" },
+  { code: 73, description: "Snow fall: Slight, moderate, and heavy intensity" },
+  { code: 75, description: "Snow fall: Slight, moderate, and heavy intensity" },
+  { code: 77, description: "Snow grains" },
+  { code: 80, description: "Rain showers: Slight, moderate, and violent" },
+  { code: 81, description: "Rain showers: Slight, moderate, and violent" },
+  { code: 82, description: "Rain showers: Slight, moderate, and violent" },
+  { code: 85, description: "Snow showers slight and heavy" },
+  { code: 86, description: "Snow showers slight and heavy" },
+  { code: 95, description: "Thunderstorm: Slight or moderate" },
+  { code: 96, description: "Thunderstorm with slight and heavy hail" },
+  { code: 99, description: "Thunderstorm with slight and heavy hail" },
+];
